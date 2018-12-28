383569af325f0e734227cfd3a72ac53341b7a333
Jeff Stephens <jeffdstephens@Jeffs-MacBook-Pro-2.local>
2018-12-28 18:38:32 -0500

    Updated navbar

--- src/components/BootstrapNavBar.js
+++ src/components/BootstrapNavBar.js
@@ -7,7 +7,7 @@ class BootstrapNavBar extends React.Component {
       <Navbar inverse collapseOnSelect>
         <Navbar.Header>
           <Navbar.Brand>
-            <a href="#brand">jeff d stephens</a>
+            <a href="http://localhost:3000">jeff d stephens</a>
           </Navbar.Brand>
           <Navbar.Toggle />
         </Navbar.Header>
