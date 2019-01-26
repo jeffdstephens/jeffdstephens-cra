3b2f96f03f64a89a3c3aa90c52b03e64c7536683
Jeff Stephens <jeffdstephens@Jeffs-MacBook-Pro-2.local>
2019-01-25 19:44:34 -0500

    Minor cleanup of files

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 import "../style.css";
-import Hero from "../components/Hero";
+
 import BootstrapNavBar from "../components/BootstrapNavBar";
 import JumboIntro from "../components/JumbotronSection";
 import AboutGrid from "../components/AboutMidGrid";
-import LandingPage from "../components/LandingPage";
 import AboutMidGrid from "../components/AboutMidGrid";
 
 class App extends Component {
@@ -13,6 +12,7 @@ class App extends Component {
       <div className="App">
         <BootstrapNavBar />
         <AboutMidGrid />
+        <JumboIntro />
       </div>
     );
   }
